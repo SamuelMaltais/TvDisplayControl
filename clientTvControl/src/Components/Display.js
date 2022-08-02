c98c3dbc9d8b31ab31d14cd92e7bec7869201a97
Samuel Maltais <leumasmontreal@hotmai.com>
2022-08-02 01:21:50 -0400

    Display final touches

--- clientTvControl/src/Components/Display.js
+++ clientTvControl/src/Components/Display.js
@@ -25,6 +25,7 @@ export default class Display extends Component {
         document.body.style.backgroundSize = "cover";
         console.log(array);
         if (!this.state.hasInterval) {
+          this.triggerOnInterval();
           this.timerID = setInterval(() => this.triggerOnInterval(), 7000);
           this.setState({ hasInterval: true });
         }
@@ -36,6 +37,9 @@ export default class Display extends Component {
   componentDidMount() {
     this.getImage();
   }
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
   triggerOnInterval = () => {
     let path =
       this.state.result[this.state.increment % this.state.result.length];
@@ -43,6 +47,9 @@ export default class Display extends Component {
       "url('http://localhost:5000/" + path + "')";
     this.setState({ increment: this.state.increment + 1 });
     console.log(this.state.result);
+    if (this.state.increment % 60 == 0) {
+      this.getImage();
+    }
   };
   changeResultString = () => {};
 
